94f94d12539e80e0b8d719f11cf05c6b32bc634c
lancelynyrd <pinedaclp@gmail.com>
2021-01-20 17:06:11 +0800

    update on sass work

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # wigo
 Withcenter Travel Application
+
+
+
+
+
+###Installing SASS Reloader
+
+Installation if npm not set
+```
+    npm init -y
+    npm i -D sass
+```
+Or Simply run `npm install` to install node packages
+```
+    npm i
+```
+
+node watch folder
+```
+ ./node_modules/.bin/sass --watch scss:css
+```
+node watch specific file
+```
+ ./node_modules/.bin/sass --watch scss/index.scss css/index.css
+```
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ( trim($_SERVER['REQUEST_URI']) != ''  && trim($_SERVER['REQUEST_URI']) != '/
 <html>
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="/wp-content/themes/wigo/index.css">
+    <link rel="stylesheet" href="/wp-content/themes/wigo/scss/index.css">
 </head>
 <body>
 
