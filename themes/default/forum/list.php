e98bd31bc944d7ecad0d0b3a947cafbf92ed16f8
YinG17 <simbulanja.17@gmail.com>
2021-01-29 15:53:59 +0800

    update pagination

--- themes/default/forum/list.php
+++ themes/default/forum/list.php
@@ -8,7 +8,7 @@ $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : 'qna';
 </div>
 <div>
     <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="notificationUnderMyPost"  v-model="alertOnNewPost" @change="onChangeAlertOnNewPost">
+        <input class="form-check-input" type="checkbox" id="notificationUnderMyPost" v-model="alertOnNewPost" @change="onChangeAlertOnNewPost">
         <label class="form-check-label" for="notificationUnderMyPost">Notification on New Post</label>
     </div>
     <div class="form-check form-switch">
@@ -20,17 +20,20 @@ $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : 'qna';
 
 <section class="post-list p-2">
     <?php
-    $posts = forum_search(['category_name' => $category, 'posts_per_page' => 20]);
+    $page_no = $_REQUEST['page_no'] ?? 1;
+    $posts_per_page = 2;
+    $offset = ($page_no - 1) * $posts_per_page;
+    $q = ['category_name' => $category, 'posts_per_page' => $posts_per_page, 'offset' => $offset];
+    $posts = forum_search($q);
 
     foreach ($posts as $post) {
         // print_r($post);
-        ?>
-
+    ?>
         <a class="d-flex justify-content-between mb-2" href="<?php echo $post['url'] ?>">
 
             <div class="d-flex">
                 <? if ( $post['profile_photo_url'] ) { ?>
-                    <img class="me-3 size-40 circle" src="<?=$post['profile_photo_url']?>">
+                <img class="me-3 size-40 circle" src="<?= $post['profile_photo_url'] ?>">
                 <? } ?>
                 <h1><?php echo $post['post_title'] ?></h1>
             </div>
@@ -39,10 +42,21 @@ $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : 'qna';
                 By <?php echo $post['author_name'] ?>
             </div>
         </a>
-
     <?php } ?>
 </section>
 
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <?php for ($i = 0; $i <= 10; $i++) {
+            $nextPage = $i + 1;
+            $nextLink = '/?page=forum/list&category=' . $category . '&page_no=' . $nextPage;
+        ?>
+            <li class="page-item"><a class="page-link" href="<?= $nextLink ?>"><?= $nextPage ?></a></li>
+        <?php } ?>
+        <li class="page-item"><a class="page-link" href="<?= $nextLink ?>">Next</a></li>
+    </ul>
+</nav>
 
 <script>
     const category = "<?php echo $category ?>";
@@ -58,28 +72,32 @@ $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : 'qna';
         data() {
             return {
                 alertOnNewPost: false,
-                alertOnNewComment:  false,
+                alertOnNewComment: false,
             }
         },
         methods: {
             onChangeAlertOnNewPost() {
                 const topic = config.post_notification_prefix + category;
-                const notificationRoute = this.$data.alertOnNewPost === true
-                    ? "notification.subscribeTopic"
-                    : "notification.unsubscribeTopic";
-                request(notificationRoute, {topic: topic}, function () {
+                const notificationRoute = this.$data.alertOnNewPost === true ?
+                    "notification.subscribeTopic" :
+                    "notification.unsubscribeTopic";
+                request(notificationRoute, {
+                    topic: topic
+                }, function() {
                     const data = {
-                        [topic]:app.alertOnNewPost ? "Y" : "N"
+                        [topic]: app.alertOnNewPost ? "Y" : "N"
                     };
                     app.onProfileMetaUpdateSubmit(data);
                 }, this.error);
             },
             onChangeAlertOnNewComment() {
                 const topic = config.comment_notification_prefix + category;
-                const notificationRoute = this.$data.alertOnNewComment === true
-                    ? "notification.subscribeTopic"
-                    : "notification.unsubscribeTopic";
-                request(notificationRoute, {topic: topic}, function () {
+                const notificationRoute = this.$data.alertOnNewComment === true ?
+                    "notification.subscribeTopic" :
+                    "notification.unsubscribeTopic";
+                request(notificationRoute, {
+                    topic: topic
+                }, function() {
                     const data = {
                         [topic]: app.alertOnNewComment ? "Y" : "N"
                     };
