1c0f4cb5e15859f7204eeb2989917228c754d0d0
JAEHO SONG <thruthesky@gmail.com>
2021-02-02 11:57:58 +0900

    update on list

--- themes/default/admin/user/edit.php
+++ themes/default/admin/user/edit.php
@@ -2,15 +2,7 @@
 $user = profile(in('ID'));
 ?>
 
-<hr>
-Admin Menu:
-<a href="/?page=admin/user-list">User list</a> |
-<a href="/?page=admin/forum-categories">Forum Categories</a> |
-<a href="/?page=admin/send-push-notification">Send Push Notifications</a> |
-Files |
-<a href="/?page=admin/translations">Translations</a>
 
-<hr>
 
 <?php
 
--- themes/default/admin/user/list.php
+++ themes/default/admin/user/list.php
@@ -1,13 +1,6 @@
 <?php
 
 ?>
-<hr>
-Admin Menu:
-<a href="/?page=admin/user/list">User list</a> |
-<a href="/?page=admin/forum/list">Forum Categories</a> |
-<a href="/?page=admin/push-notification/send">Send Push Notifications</a> |
-Files |
-<a href="/?page=admin/translations">Translations</a>
 
 <hr>
 <h1>Admin User List</h1>
