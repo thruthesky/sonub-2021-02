4f819f1abec1dd31f6076e63c9a4a9e75e348706
JAEHO SONG <thruthesky@gmail.com>
2021-02-13 18:35:06 +0900

    update for cafe setting

--- widgets/login/social-login/social-login.php
+++ widgets/login/social-login/social-login.php
@@ -5,8 +5,7 @@ $o = get_widget_options();
     <div class="fs-xs">
         카카오톡 또는 네이버 로그인을 해주세요.
     </div>
-
-    <img class="mt-1 w-100" src="/wp-content/themes/sonub/themes/sonub/img/kakao-login.png">
-    <img class="mt-1 w-100" src="/wp-content/themes/sonub/themes/sonub/img/naver-login.png?v=4">
+    <img class="mt-1 w-100" src="/wp-content/themes/sonub/widgets/login/social-login/kakao-login.png">
+    <img class="mt-1 w-100" src="/wp-content/themes/sonub/widgets/login/social-login/naver-login.png?v=4">
 </div>
 
