ae3c5a342c0d8a0ccf72a8feb139b2e4a110493f
JAEHO SONG <thruthesky@gmail.com>
2021-02-22 17:44:45 +0900

    reminder

--- themes/sonub/home.php
+++ themes/sonub/home.php
@@ -5,15 +5,17 @@ if ( is_in_cafe() == false ) {
 
     include widget('posts/two-columns', [
         'left' => [
-            'category_name' => 'qna',
+            'category_name' => 'reminder',
         ],
         'middle' => [
-            'category_name' => 'reminder',
+            'category_name' => 'qna',
         ],
         'class' => 'border-radius-md',
     ]);
 
-    include widget('posts/latest-photos', ['category_name' => 'qna', 'posts_per_page' => 4, 'widget_title' => '질문과 답변 사진', 'class' => "mt-2 border-radius-md"]);
+    include widget('posts/latest-photos', [
+        'category_name' => 'qna',
+        'posts_per_page' => 4, 'widget_title' => '질문과 답변 사진', 'class' => "mt-2 border-radius-md"]);
     include widget('posts/latest', ['category_name' => 'reminder', 'widget_title' => '공지사항', 'class' => "mt-2 border-radius-md"]);
     include widget('posts/latest', ['category_name' => 'qna', 'widget_title' => '질문과 답변', 'class' => "mt-2 border-radius-md"]);
     include widget('posts/latest', ['category_name' => 'discussion', 'widget_title' => '자유게시판', 'class' => "mt-2 border-radius-md"]);
